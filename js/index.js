61408a9ef090bb8839ae2a49aafd280cd1b6c656
Isaac Fisher <isaac.fisher@nyu.edu>
2024-06-11 18:16:15 -0400

    only show incorrect name once to avoid annoyance

--- js/index.js
+++ js/index.js
@@ -1,15 +1,23 @@
-var corrected = 0;
-
+function onPageLoad(){
+	if (localStorage.getItem("corrected") != "1") {
+		title = document.getElementById("name-title");
+		title.style.textDecoration = "underline";
+		title.style.textDecorationThickness = ".25rem";
+		title.style.textDecorationStyle = "wavy";
+		title.style.textDecorationColor = "red";
+		title.innerHTML = "issac";
+	}
+}
 function titleOnClick(){
 	title = document.getElementById("name-title");
 	title.style.textDecoration = "none";
 	title.innerHTML = "isaac";
-	corrected = 1;
+	localStorage.setItem("corrected", "1");
 	title.style.backgroundColor = "transparent";
 	title.style.cursor = "auto"
 }
 function titleOnHover(){
-	if (corrected == 0) {
+	if (localStorage.getItem("corrected") != "1") {
 		title = document.getElementById("name-title");
 		title.style.backgroundColor = "#ffa187";
 		title.style.cursor = "pointer";
