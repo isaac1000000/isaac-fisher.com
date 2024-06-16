fcc5734ca4e92393da449db9ac84cd55f1c42007
Isaac Fisher <isaac.fisher@nyu.edu>
2024-06-16 16:29:38 -0400

    improvements to mobile menu and small fixes

--- js/core.js
+++ js/core.js
@@ -1,9 +1,12 @@
 function dropMenu() {
 	var dropdownContent = document.getElementsByClassName("menu-content")[0];
 	var dropdownDisplay = window.getComputedStyle(dropdownContent, null).display;
+	var menuButton = document.querySelector(".menu-btn img");
 	if (dropdownDisplay == "none") {
 		dropdownContent.style.display = "block";
+		menuButton.src = "/imgs/exit-menu.svg";
 	} else {
 		dropdownContent.style.display = "none";
+		menuButton.src = "/imgs/menu.svg";
 	}
 }
