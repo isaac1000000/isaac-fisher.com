84eed6995101c5e8635d9c3c27e8edcb6b95c673
Isaac Fisher <isaac.fisher@nyu.edu>
2024-06-17 22:13:22 -0400

    scroll animations on home page

--- js/index.js
+++ js/index.js
@@ -27,3 +27,53 @@ function titleOnLeave(){
 	title = document.getElementById("name-title");
 	title.style.backgroundColor = "transparent";
 }
+function setupScrollFadeIn(){
+	const scrollOffset = 200;
+
+	const scrollElements = document.querySelectorAll(".scroll-fade-in");
+
+	scrollElements.forEach((el) => {
+		el.style.opacity = 0;
+	});
+
+	window.addEventListener('scroll', () => {
+  		handleScrollAnimation();
+	});
+}
+
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutOfView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  const scrollElements = document.querySelectorAll(".scroll-fade-in");
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutOfView(el)){
+      hideScrollElement(el)
+    }
+  })
+}
