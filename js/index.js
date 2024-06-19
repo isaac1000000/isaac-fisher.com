5685d6d6799597ac8ef620d5f75a2195738b4739
Isaac Fisher <isaac.fisher@nyu.edu>
2024-06-19 17:07:02 -0400

    sketch box

--- js/index.js
+++ js/index.js
@@ -48,3 +48,44 @@ const handleScrollAnimation = () => {
     }
   })
 }
+
+const sketchSrcs = [
+  "/imgs/sketches/sketch.jpeg",
+  "/imgs/sketches/sketch2.jpg",
+  "/imgs/sketches/sketch3.jpg",
+  "/imgs/sketches/sketch4.jpg",
+  "/imgs/sketches/sketch5.jpg",
+  "/imgs/sketches/sketch6.jpg",
+  ];
+var sketchIndex = 0;
+
+function sketchLeftOnClick() {
+  sketchIndex -= 1;
+  if (sketchIndex < 0) {
+    sketchIndex += sketchSrcs.length
+  }
+  setSketchIndex(sketchIndex);
+}
+
+function sketchRightOnClick() {
+  sketchIndex += 1;
+  if (sketchIndex >= sketchSrcs.length) {
+    sketchIndex -= sketchSrcs.length
+  }
+  setSketchIndex(sketchIndex);
+}
+
+function setSketchIndex(index) {
+  sketchIndex = index;
+  const sketchImg = document.getElementById("sketch");
+  const indexBadges = document.querySelectorAll(".sketch-index-badge");
+
+  sketchImg.src = sketchSrcs[index];
+  indexBadges.forEach((badge, badgeIndex) => {
+    if (badgeIndex != index) {
+      badge.classList.remove("active");
+    } else {
+      badge.classList.add("active");
+    }
+  })
+}
